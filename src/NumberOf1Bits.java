cf3dcecacc1e9388a347a3ae1efdd548d967ca46
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 22:57:28 -0700

    check the number of 1s in the bit

--- src/NumberOf1Bits.java
+++ src/NumberOf1Bits.java
@@ -1,5 +1,13 @@
 public class NumberOf1Bits {
-    
+    public static int numberOf1Bits(int n){
+        int count =0;
+        while(n>0){
+            if((n&1)==1){
+                count ++;
+            }
+            n >>=1;
+        }return count;
+    }
 
     public static void main(String[] args) {
 
