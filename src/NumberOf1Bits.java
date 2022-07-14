3dc7b0cb4c74b2fd861936162410bf5a9d78c9b0
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-13 22:59:20 -0700

    added some commments, but i am not sure why when i try to add the else statement its giving me time limit exceeded error??

--- src/NumberOf1Bits.java
+++ src/NumberOf1Bits.java
@@ -1,10 +1,16 @@
 public class NumberOf1Bits {
+
+
     public static int numberOf1Bits(int n){
+        //initially count is 0
         int count =0;
+        //until n becomes 0
         while(n>0){
+            // if its one then increment count
             if((n&1)==1){
                 count ++;
             }
+            //else just move n to right side by one
             n >>=1;
         }return count;
     }
